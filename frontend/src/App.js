4887b73cff82816257f98492b9ab03e4c59b7f74
hatim <hdamra@gmail.com>
2022-03-01 16:26:16 -0600

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import {
+  About,
+  Footer,
+  Header,
+  Skills,
+  Testimonial,
+  Work
+} from './container';
+
+import {
+  Navbar
+} from './components';
+
+import './App.scss';
+
+export const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
